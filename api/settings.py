877d6835be91035066bd5e00fc6e847901a4f934
Paulo Roberto Tassinari Ignacio <72086403+paulortignacio@users.noreply.github.com>
2025-01-18 04:42:29 -0300

    Update settings.py

--- api/settings.py
+++ api/settings.py
@@ -89,11 +89,11 @@ WSGI_APPLICATION = 'api.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('DB_NAME', 'corridas'),
-        'USER': os.getenv('DB_USER', 'corridas'),
-        'PASSWORD': os.getenv('DB_PASSWORD', 'dbuserpass'),
-        'HOST': os.getenv('DB_HOST', 'db'),
-        'PORT': os.getenv('DB_PORT', '3306'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
